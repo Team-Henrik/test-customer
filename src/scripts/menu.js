921897a8383fa990093263947a2b83259c34effe
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-06-13 07:53:27 +0200

    responsive navigation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,10 +1,21 @@
 const btn = document.querySelector(".hamburger");
-const menu = document.querySelector("nav ul");
+const menu = document.querySelector("nav");
+const iconHamburger = btn.querySelector('.icon-hamburger');
+const iconClose     = btn.querySelector('.icon-close');
 
 if (btn && menu) {
   btn.addEventListener("click", () => {
     const isOpen = btn.getAttribute("aria-expanded") === "true";
     btn.setAttribute("aria-expanded", String(!isOpen));
     menu.classList.toggle("hidden");
+    if (!menu.classList.contains("hidden")) {
+      menu.classList.add("flex")
+      iconHamburger.classList.add("hidden")
+      iconClose.classList.remove("hidden")
+    } else {
+      menu.classList.remove("flex");
+      iconClose.classList.add("hidden");
+      iconHamburger.classList.remove("hidden");
+    }
   })
 }
