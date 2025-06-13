9867fa8e16bdea1e6894fa2374fe4e8dc154258f
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-06-13 04:08:24 +0200

    install astro icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://testkunde.netlify.app/",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()]
 });
